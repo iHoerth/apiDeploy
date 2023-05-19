78077a46110e6e03eeb6e4b57c8db367ecab3671
iHoerth <hoerth.ignacio@gmail.com>
2023-05-19 17:41:32 -0300

    LISTO EL BACK CARALHO PORRA

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,14 +17,14 @@ const { createFakeData } = require("../fakeData/fakeData");
 
 const router = Router();
 
-router.post("/fake", async (req, res) => {
-  try {
-    await createFakeData();
-    return res.status(200).send("data created");
-  } catch (error) {
-    return res.status(404).send(error.message);
-  }
-});
+// router.post("/fake", async (req, res) => {
+//   try {
+//     await createFakeData();
+//     return res.status(200).send("data created");
+//   } catch (error) {
+//     return res.status(404).send(error.message);
+//   }
+// });
 
 router.use("/doctors", doctors);
 router.use("/patients", patients);
