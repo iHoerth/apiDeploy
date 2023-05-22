f3e0e575b8598f78404bbb09361728e06d050b8d
iHoerth <hoerth.ignacio@gmail.com>
2023-05-21 21:46:37 -0300

    Back 100% listo

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ const cors = require('cors');
 const routes = require('./src/routes/index');
 const { conn } = require('./src/db');
 
-const mercadopago = require('mercadopago');
-
-const hostFront = DEPLOY || 'http://localhost';
-const portFront = DEPLOYPORT || 3000;
+const hostFront = DEPLOY || 'localhost';
+const portFront = DEPLOYPORT || ':3000';
 
 const port = PORT || 3001;
 
@@ -22,6 +20,7 @@ server.use(
     origin: ['*', 'https://apideploy-production.up.railway.app'],
   })
 );
+
 
 // server.use(
 //   cors({
@@ -53,9 +52,9 @@ server.post('/turno', (req, res) => {
   let preference = {
     //urls donde te redirige en cada caso, pago exitoso, pendiente y fallo, x razon
     back_urls: {
-      success: `${hostFront}:${portFront}/turno/${dataPreferences.idDoctor}/${dataPreferences.fechaCita}/${dataPreferences.horaCita}/aprobado/${dataPreferences.comentario}`,
+      success: `http://${hostFront}${portFront}/turno/${dataPreferences.idDoctor}/${dataPreferences.fechaCita}/${dataPreferences.horaCita}/aprobado/${dataPreferences.comentario}`,
       // pending: `http://localhost:3000/turno/${dataPreferences.idDoctor}/${dataPreferences.fechaCita}/${dataPreferences.horaCita}/pendiente/`,
-      failure: `${hostFront}:${portFront}/turno/${dataPreferences.idDoctor}/${dataPreferences.fechaCita}/${dataPreferences.horaCita}/rechazado/0`,
+      failure: `http://${hostFront}${portFront}/turno/${dataPreferences.idDoctor}/${dataPreferences.fechaCita}/${dataPreferences.horaCita}/rechazado/0`,
     },
     items: [
       {
@@ -98,6 +97,7 @@ server.post('/turno', (req, res) => {
 });
 
 // SDK de Mercado Pago
+const mercadopago = require('mercadopago');
 // Agrega credenciales
 mercadopago.configure({
   access_token: tokenMP,
